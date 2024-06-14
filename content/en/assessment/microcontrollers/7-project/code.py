5713064b05622b0d1d00886e318f15f79761b1fe
Alistair Knight <alistair.knight@gmail.com>
2024-06-14 14:02:24 +0930

    Updated code.py for 2024, added design options ppt.

--- content/en/assessment/microcontrollers/7-project/code.py
+++ content/en/assessment/microcontrollers/7-project/code.py
@@ -1,35 +1,50 @@
 import board, neopixel, time, digitalio
 
-
+# Set up light strip
 pixpin = board.GP28
 numpix = 3
 pixels = neopixel.NeoPixel(
-    pixpin, numpix, brightness=1, auto_write=True, pixel_order=neopixel.GRBW
+    pixpin, numpix, brightness=0.3, auto_write=True, pixel_order=neopixel.GRB
 )
 
-sensor_pin = board.GP18
-# Enable internal pullup resistor on sensor pin
+# Set up light sensor pin
+sensor_pin = board.GP16
 pin = digitalio.DigitalInOut(sensor_pin)
 pin.direction = digitalio.Direction.INPUT
 pin.pull = digitalio.Pull.UP
 
-RED = (200, 0, 0, 0)
-GREEN = (0, 255, 0, 0)
-BLUE = (0, 0, 200, 0)
-WHITE = (0, 0, 0, 200)
-OFF = (0, 0, 0, 0)
+# Define our custom colours
+# (r, g, b) with each value 0-255
+# Google "color picker" for ideas!
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+OFF = (0, 0, 0)
 
 while True:
-    print(pin.value)
+    # Print a dot to check that it is going around the loop
+    # Check this with the serial monitor if things aren't working
+    print('.', end='')
     if pin.value:
-        pixels.fill(RED)
-        time.sleep(2)
-        pixels.fill(GREEN)
-        time.sleep(2)
-        pixels.fill(BLUE)
-        time.sleep(2)
+        # It's dark
+        pixels[0]= RED
+        time.sleep(0.25)
+        pixels[1] = GREEN
+        time.sleep(0.25)
+        pixels[2] = BLUE
+        time.sleep(0.25)
         pixels.fill(WHITE)
-        time.sleep(2)
+        time.sleep(0.25)
 
     else:
+        # It's light
         pixels.fill(OFF)
+        pixels[0] = (10, 10, 10)
+        time.sleep(0.1)
+        pixels.fill(OFF)
+        pixels[1] = (10, 10, 10)
+        time.sleep(0.1)
+        pixels.fill(OFF)
+        pixels[2] = (10, 10, 10)
+        time.sleep(0.1)
